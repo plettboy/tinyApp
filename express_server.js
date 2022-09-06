3e6fd9d8ed48dce7f3960440f0446cbcdb2e46b0
Ryan Plett <108938981+plettboy@users.noreply.github.com>
2022-09-05 19:41:33 -0700

    Add files via upload
    updated number and letter generators

--- express_server.js
+++ express_server.js
@@ -1,50 +1,88 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = 8080; // default port 8080
+
 
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-app.set("view engine", "ejs");
+const generateRandomString = function() {
+  let result = "";
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  for (let i = 6; i > 0; i++) {
+    const random = chars.charAt(Math.floor(Math.random() * 62));
+    result += random;
+  }
+  return result;
+}
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xk': 'http://www.google.com'
 };
 
-//example of a get request/route
-app.get("/", (req, res) => {
-  res.send("Hello!");
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
 
-app.get("/urls.json", (req, res) => {
+app.get('/u/:id', (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+})
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  const longURL = req.body.longURL;
+
+  if (!longURL) {
+    return res.status(400).send('Enter a valid request.');
+  }
+
+  const id = generateRandomString();
+  urlDatabase[id] = longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${id}`);
+});
+
+app.post('/urls/:id/update', (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect(`/urls`);
+})
+
+app.post('/urls/:id/delete', (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect(`/urls`);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.get('/urls/:id', (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+
+  if (!longURL) {
+    return res.status(400).send("URL doesn't exist!");
+  }
+
+  const templateVars = { id, longURL };
+  res.render('urls_show', templateVars);
+});
+
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello!<b>World</b></body></html>\n');
 });
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id]
-  const templateVars = { id: id, longURL: longURL };
-  res.render("urls_show", templateVars);
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
