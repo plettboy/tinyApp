70395dfe0f769ac0b0950f03171039ea322f8f3c
Ryan Plett <108938981+plettboy@users.noreply.github.com>
2022-09-11 11:50:31 -0700

    Add files via upload
    "commit history located in the master branch, main branch is being utilized as master branch has a sub module that is not deletable."

---
+++ README.md
@@ -0,0 +1,25 @@
+# TinyApp Project
+
+TinyApp is a full stack web application built with Node and Express that allows users to shorten long URLs (à la bit.ly).
+
+## Final Product
+
+### URLs Page
+!["Screenshot of URLs page"](https://github.com/plettboy/tinyApp/blob/master/docs/urls.png?raw=true)
+### Account Registration
+!["Screenshot of the Registration Page"](https://github.com/plettboy/tinyApp/blob/master/docs/register_page.png?raw=true)
+### Addition of New URL's
+!["screenshot URL Addition Page"](https://github.com/plettboy/tinyApp/blob/master/docs/new_url_page.png?raw=true)
+
+## Dependencies
+
+- Node.js
+- Express
+- EJS
+- bcryptjs
+- cookie-session
+
+## Getting Started
+
+- Install all dependencies (using the `npm install` command).
+- Run the development web server using the `node express_server.js` command.
--- express_server.js
+++ express_server.js
@@ -1,28 +1,46 @@
 const express = require('express');
-var cookieParser = require('cookie-parser')
+const cookieSession = require('cookie-session');
+const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 8080; // default port 8080
 
 
+const {generateRandomString, findUserByEmail, getUserFromCookie, getURLSofUser, emailMatch} = require("./helpers.js")
+console.log(findUserByEmail())
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieSession({
+  name: 'session',
+  keys: ['secretkey'],
+}))
 app.set('view engine', 'ejs');
 
-//this generates a random string id
-const generateRandomString = function() {
-  let result = '';
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
-  for (let i = 6; i > 0; i--) {
-    const randChar = chars.charAt(Math.floor(Math.random() * chars.length));
-    result += randChar;
-  }
-  return result;
-}
-
-//our database for urls to start
+//our updated database
 const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xk': 'http://www.google.com'
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "b2x3rt",
+  },
+};
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
 };
 
 //this is simply the example page
@@ -31,57 +49,145 @@ app.get('/', (req, res) => {
 });
 
 app.get('/u/:id', (req, res) => {
-  const longURL = urlDatabase[req.params.id];
+  // incorrect id entered, so we send an error 
+  console.log(req)
+  const id = req.params.id;
+  const urlID = urlDatabase[id];
+  if (!urlID) {
+    return res.status(400).send("That id doesn't exist!");
+  }
+  const longURL = urlDatabase[req.params.id].longURL;
   res.redirect(longURL);
 })
 
 //whenever we see app.get and then a render request we are asking html to render the html webpage.
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] }; //to get the username to show up we added the username cookie as a paramter.
-  res.render('urls_index', templateVars);
+  const userID = (req.session["user_id"])
+  const user = getUserFromCookie(userID, users)
+const myURLS = getURLSofUser(userID, urlDatabase)
+
+const templateVars = { urls: myURLS, user }; //to get the username to show up we added the username cookie as a paramter.
+
+if(!userID) {
+  return res.send("You need to login!")
+}
+res.render('urls_index', templateVars);
 });
 
-//this is meant for entering 
+app.get('/login', (req, res) => {
+  if (req.session['user_id']) {
+    res.redirect(`/urls`);
+  } 
+
+  const templateVars = {user: null}
+  res.render("login", templateVars)
+});
+
+app.post('/login', (req, res) => {
+const email = req.body.email;
+const password = req.body.password;
+
+//error handling, if user and pass are zero return error
+if (email.length === 0 && password.length === 0) {
+  return res.status(400).send(`400 error - Missing E-mail or Password`);
+}
+//if emailmatch is true, then error
+const user = findUserByEmail(email, users)
+if (!user) {
+  return res.status(400).send(`400 error - No user found!`);
+}
+if (!bcrypt.compareSync(password, user.password)) {
+  return res.status(400).send(`400 error - Incorrect email or password!`);
+}
+
+if (user) {
+
+  req.session.user_id = user.id
+  res.redirect(`/urls`);
+
+} else {
+  res.redirect('/login')
+}
+})
+
 app.post('/urls', (req, res) => {
   console.log(req.body);
   const longURL = req.body.longURL;
-  if (!longURL) {
 
+  if (!longURL) {
+    
     return res.status(400).send('Enter valid url!'); 
   }
 
+  const user = getUserFromCookie(req.session["user_id"], users)
+  if(!user) {
+    return res.status(400).send('NO! You are not logged in!'); 
+  }
+
   const id = generateRandomString();
-  urlDatabase[id] = longURL;
+  urlDatabase[id] = {
+    longURL,
+    userID: req.session["user_id"]
+  }
   console.log(urlDatabase);
   res.redirect(`/urls/${id}`);
 });
 
-app.post('/urls/:id/update', (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
+app.post('/urls/:id', (req, res) => {
+  urlDatabase[req.params.id].longURL = req.body.longURL;
   res.redirect(`/urls`);
 })
 
-//this is a post and not a delete because it does not take us to new page, it posts and deletes the thing
 app.post('/urls/:id/delete', (req, res) => {
+
+  // if user doesn't own url
+  if (urlDatabase[req.params.id].userID !== req.session.user_id) {
+    return res.status(403).send(`You don't own that URL.`);
+  }
+
   delete urlDatabase[req.params.id];
   //once you delete, redirect to the url page
   res.redirect(`/urls`);
 });
 
 app.get('/urls/new', (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+const user = getUserFromCookie(req.session["user_id"], users)
+  if(!user) {
+    res.redirect('/login')
+  }
+
+  const templateVars = { user };
   res.render('urls_new', templateVars);
 });
 
+//this is a get request for a new register page, user name is null and we render the file /register
+app.get('/register', (req, res) => {
+  const templateVars = { user:getUserFromCookie(null, users) };
+  res.render('register', templateVars);
+});
+
 app.get('/urls/:id', (req, res) => {
+  if (!req.session['user_id']) {
+    return res.status(403).send(`You are not logged in!`)
+  } 
+
   const id = req.params.id;
-  const longURL = urlDatabase[id];
+  if (!urlDatabase[id]) {
+    return res.status(403).send(`ID not present in database!`)
+  }
+
+  let urlsOfUser = getURLSofUser(req.session['user_id'], urlDatabase)
+  if (!urlsOfUser[id]) {
+    return res.status(403).send(`You do not own this ID!`)
+  }
+
+  const longURL = urlDatabase[id].longURL;
 
   if (!longURL) {
     return res.status(400).send("URL doesn't exist!");
   }
 
-  const templateVars = { id, longURL, username: req.cookies["username"] };
+  const templateVars = { id, longURL, user:getUserFromCookie(req.session["user_id"], users) };
   res.render('urls_show', templateVars);
 });
 
@@ -93,16 +199,39 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello!<b>World</b></body></html>\n');
 });
 
-//requests the username, and assigns it to a cookie
-app.post('/login', (req, res) => {
-  res.cookie("username", req.body.username)
-  res.redirect(`/urls`);
-})
-
 //clears the username cookie from memory
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+req.session = null;
   res.redirect("/urls");
+});
+
+// this connects the forms in register.ejs to our server
+app.post('/register', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  //error handling, if user and password are zero return error
+  if (email.length === 0 || password.length === 0) {
+    return res.status(400).send(`400 error - Missing E-mail or Password`);
+  }
+
+// if emailmatch is true, then error
+  if (findUserByEmail(email, users)) {
+    return res.status(400).send(`400 error - A new email is required.`);
+
+  }
+
+  const id = generateRandomString()
+
+
+  //hashed passowrd added in to the password property
+  const user = { email, password: hashedPassword, id }
+
+  users[id] = user;
+  req.session.user_id = id;
+  res.redirect(`/urls`);
 });
 
 app.listen(PORT, () => {
